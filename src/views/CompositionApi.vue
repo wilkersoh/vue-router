d41d4f6d82e0eb4db921d6a6097910a154595755
wilker <wilker.soh@growthops.asia>
2021-04-04 12:13:45 +0800

    Update computed vuejs note

--- src/views/CompositionApi.vue
+++ src/views/CompositionApi.vue
@@ -7,16 +7,22 @@
   <h2>Reactive</h2>
   <p>{{ userOther.name }} and age is {{ userOther.age }}</p>
   <button @click="updateUserOther">Update other user</button>
+  <h2>Computed hook to use filter</h2>
+  <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+  <input type="text" v-model="search" />
+  <p>search term - {{ search }}</p>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
 export default {
   name: 'CompositionApi',
   setup() {
     console.log('setup first');
     const user = ref({ name: 'yee', age: 30 });
+    const search = ref('');
+    const names = ref(['yee', 'laoyeche', 'wilker']);
 
     const userOther = reactive({ name: 'laoyeche', age: 25 });
 
@@ -24,13 +30,38 @@ export default {
       user.value.age = 40;
     };
     const updateUserOther = () => {
-      userOther.age = 10; // didn't use .value
+      userOther.age = 10; // dont't need to use .value
     };
 
     // reactive cannot use with Primitive value. it wouldn't change from the view.
     // Note: use ref is better.
 
-    return { user, userOther, updateUser, updateUserOther };
+    // computed
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => {
+        /**
+         * default: "wilker".includes("") > true
+         * Flow:
+         * 1. Run js first
+         * 2. return matchingNames.value value to dom if result is true
+         * 3. Dom value input changed, it didn't re-render,
+         * 4. it will computed it in client side for this names.value.filter function
+         * 5. instead of re-render.
+         *
+         */
+        const result = name.includes(search.value);
+        return result;
+      });
+    });
+
+    return {
+      user,
+      userOther,
+      updateUser,
+      updateUserOther,
+      matchingNames,
+      search,
+    };
   },
   created() {
     console.log('created second');
