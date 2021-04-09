ca94948c90ada163afd249a370e8d7c5fb3ba658
wilkersoh <plgu@hotmail.com>
2021-04-09 17:08:21 +0800

    Setup Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   variants: {
