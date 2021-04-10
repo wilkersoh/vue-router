2966bb73e35d43c16a05705733a0f2ba6e206c07
wilkersoh <plgu@hotmail.com>
2021-04-10 15:07:08 +0800

    Update Custom directive

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import PassParamsFromRoute from '../views/passParams/PassParamsFromRoute.vue';
-import OtherPage from '../views/passParams/OtherPage.vue';
-import NotFound from '../views/NotFound.vue';
-import CompositionApi from '../views/CompositionApi.vue';
-import Composables from '../views/posts/Composables.vue';
-import PostDetails from '../views/posts/PostDetails.vue';
-import FormVue from '../views/FormVue.vue';
-import DynamicComponent from '../views/DynamicComponent.vue';
-import TeleportVue from '../views/TeleportVue.vue';
-import MixinsOrHook from '../views/MixinsOrHook.vue';
-import WatchVue from '../views/WatchVue.vue';
-import TemplateRef from '../views/TemplateRef.vue';
-import PropsWithComposition from '../views/PropsWithComposition.vue';
-import VeeValidateForm from '../views/VeeValidateForm.vue';
-import DefineComponentVue from '../views/DefineComponentVue.vue';
-import MutilpleComponentInOneFile from '../views/MutilpleComponentInOneFile.vue';
-import JSXComponent from '../views/JSXComponent.vue';
-import LazyLoadingComponent from '../views/LazyLoadingComponent.vue';
-import HRender from '../views/HRender.vue';
-import FormValidateYup from '../views/form';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import PassParamsFromRoute from "../views/passParams/PassParamsFromRoute.vue";
+import OtherPage from "../views/passParams/OtherPage.vue";
+import NotFound from "../views/NotFound.vue";
+import CompositionApi from "../views/CompositionApi.vue";
+import Composables from "../views/posts/Composables.vue";
+import PostDetails from "../views/posts/PostDetails.vue";
+import FormVue from "../views/FormVue.vue";
+import DynamicComponent from "../views/DynamicComponent.vue";
+import TeleportVue from "../views/TeleportVue.vue";
+import MixinsOrHook from "../views/MixinsOrHook.vue";
+import WatchVue from "../views/WatchVue.vue";
+import TemplateRef from "../views/TemplateRef.vue";
+import PropsWithComposition from "../views/PropsWithComposition.vue";
+import VeeValidateForm from "../views/VeeValidateForm.vue";
+import DefineComponentVue from "../views/DefineComponentVue.vue";
+import MutilpleComponentInOneFile from "../views/MutilpleComponentInOneFile.vue";
+import JSXComponent from "../views/JSXComponent.vue";
+import LazyLoadingComponent from "../views/LazyLoadingComponent.vue";
+import HRender from "../views/HRender.vue";
+import FormValidateYup from "../views/form";
+import DirectiveCustom from "../views/DirectiveCustom";
 
 export const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: '/pass-params-from-route',
-    name: 'PassParamsFromRoute',
+    path: "/pass-params-from-route",
+    name: "PassParamsFromRoute",
     component: PassParamsFromRoute,
     // children: [
     //   {
@@ -50,8 +51,8 @@ export const routes = [
     // ],
   },
   {
-    path: '/jobs/:id',
-    name: 'OtherPage',
+    path: "/jobs/:id",
+    name: "OtherPage",
     component: OtherPage,
     props: true, // pass params as props if you turn on this
     beforeEnter: (to, from, next) => {
@@ -60,100 +61,105 @@ export const routes = [
     },
   },
   {
-    path: '/composition-api',
-    name: 'CompositionApi',
+    path: "/composition-api",
+    name: "CompositionApi",
     component: CompositionApi,
   },
   {
-    path: '/composition-api/:id',
-    name: 'PostDetails',
+    path: "/composition-api/:id",
+    name: "PostDetails",
     component: PostDetails,
     props: true,
   },
   {
-    path: '/composables',
-    name: 'Composables',
+    path: "/composables",
+    name: "Composables",
     component: Composables,
   },
   {
-    path: '/form',
-    name: 'FormVue',
+    path: "/form",
+    name: "FormVue",
     component: FormVue,
   },
   {
-    path: '/dynamic-component',
-    name: 'DynamicComponent',
+    path: "/dynamic-component",
+    name: "DynamicComponent",
     component: DynamicComponent,
   },
   {
-    path: '/teleport',
-    name: 'TeleportVue',
+    path: "/teleport",
+    name: "TeleportVue",
     component: TeleportVue,
   },
   {
-    path: '/mixins-or-hook',
-    name: 'MixinsOrHook',
+    path: "/mixins-or-hook",
+    name: "MixinsOrHook",
     component: MixinsOrHook,
   },
   {
-    path: '/watch',
-    name: 'WatchVue',
+    path: "/watch",
+    name: "WatchVue",
     component: WatchVue,
   },
   {
-    path: '/template-ref',
-    name: 'TemplateRef',
+    path: "/template-ref",
+    name: "TemplateRef",
     component: TemplateRef,
   },
   {
-    path: '/props-composition',
-    name: 'PropsWithComposition',
+    path: "/props-composition",
+    name: "PropsWithComposition",
     component: PropsWithComposition,
   },
   {
-    path: '/vee-validate',
-    name: 'VeeValidateForm',
+    path: "/vee-validate",
+    name: "VeeValidateForm",
     component: VeeValidateForm,
   },
   {
-    path: '/define-component',
-    name: 'DefineComponentVue',
+    path: "/define-component",
+    name: "DefineComponentVue",
     component: DefineComponentVue,
   },
   {
-    path: '/multiple-component-in-file',
-    name: 'MutilpleComponentInOneFile',
+    path: "/multiple-component-in-file",
+    name: "MutilpleComponentInOneFile",
     component: MutilpleComponentInOneFile,
   },
   {
-    path: '/jsx-component',
-    name: 'JSXComponent',
+    path: "/jsx-component",
+    name: "JSXComponent",
     component: JSXComponent,
   },
   {
-    path: '/lazy-loading-component',
-    name: 'LazyLoadingComponent',
+    path: "/lazy-loading-component",
+    name: "LazyLoadingComponent",
     component: LazyLoadingComponent,
   },
   {
-    path: '/h-render',
-    name: 'HRender',
+    path: "/h-render",
+    name: "HRender",
     component: HRender,
   },
   {
-    path: '/validate-with-yup',
-    name: 'FormValidateYup',
+    path: "/validate-with-yup",
+    name: "FormValidateYup",
     component: FormValidateYup,
+  },
+  {
+    path: "/directive-custom",
+    name: "DirectiveCustom",
+    component: DirectiveCustom,
   },
   // redirect
   {
-    path: '/all-jobs',
-    redirect: '/jobs',
+    path: "/all-jobs",
+    redirect: "/jobs",
   },
   // catch all 404
   {
-    path: '/:catchAll(.*)',
-    name: 'NotFound',
+    path: "/:catchAll(.*)",
+    name: "NotFound",
     component: NotFound,
   },
 ];
---
+++ src/views/DirectiveCustom.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div>
+    <h1>Custom Directive</h1>
+    <div v-pin> i am directive</div>
+  </div>
+</template>
+
+<script>
+const PinDirective = {
+  beforeMount(el, binding, vnode, prevVnode) {
+    el.style.position = "absolute";
+    el.style.right = " 120px";
+    el.style.color = " red";
+  },
+}
+
+
+export default {
+  name : 'DirectiveCustom',
+  directives: {pin: PinDirective}
+}
+</script>
+
