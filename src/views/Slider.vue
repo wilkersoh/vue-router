829931c658dfb8b2d0eeca9e83e057454b434e84
wilkersoh <plgu@hotmail.com>
2021-04-12 20:58:49 +0800

    Update slider

--- src/views/Slider.vue
+++ src/views/Slider.vue
@@ -2,72 +2,83 @@
   <div>
     <h1>I am slider</h1>
     <div class="slider">
-      <ul class="sliderContent">
+      <ul class="sliderContent" :style="{ transformStyle }">
         <!-- <li v-for="(image, index) in images" :key="index"> -->
-          <img v-for="(image, index) in images" :key="index" :src="image" />
+        <img v-for="(image, index) in images" :key="index" :src="image" />
         <!-- </li> -->
       </ul>
+      <button @click="prev" class="button btn-previous">previous</button>
+      <button @click="next" class="button btn-next">next</button>
     </div>
   </div>
 </template>
 
 <script>
-import {ref, reactive, toRefs, watch} from 'vue';
+import { ref, reactive, toRefs, watch } from 'vue';
 
-  export default {
-    name: 'Slider',
-    setup() {
-      const images = ref([
-        "https://cdn.pixabay.com/photo/2015/12/12/15/24/amsterdam-1089646_1280.jpg",
-        "https://cdn.pixabay.com/photo/2016/02/17/23/03/usa-1206240_1280.jpg",
-        "https://cdn.pixabay.com/photo/2015/05/15/14/27/eiffel-tower-768501_1280.jpg",
-        "https://cdn.pixabay.com/photo/2016/12/04/19/30/berlin-cathedral-1882397_1280.jpg"
-      ]);
+export default {
+  name: 'Slider',
+  setup() {
+    const images = ref([
+      'https://cdn.pixabay.com/photo/2015/12/12/15/24/amsterdam-1089646_1280.jpg',
+      'https://cdn.pixabay.com/photo/2016/02/17/23/03/usa-1206240_1280.jpg',
+      'https://cdn.pixabay.com/photo/2015/05/15/14/27/eiffel-tower-768501_1280.jpg',
+      'https://cdn.pixabay.com/photo/2016/12/04/19/30/berlin-cathedral-1882397_1280.jpg',
+    ]);
 
-      const state = reactive({
-        currentIndex: 0,
-        timer: null,
-      })
+    const state = reactive({
+      currentIndex: 0,
+      transition: 0.45,
+    });
 
-      const initSlide = () => {
+    const transformStyle = ref('');
 
+    const prev = () => {
+      if (state.currentIndex === 0) return;
+      state.currentIndex -= 1;
+      console.log('click prev');
+    };
+
+    const next = () => {
+      if (state.currentIndex === images.length - 1) return;
+      state.currentIndex += 1;
+      console.log('click next');
+    };
+
+    watch(
+      () => state.currentIndex,
+      (newIndex, prevIndex) => {
+        const value = newIndex * 100;
+        // transformStyle.value = `transform: translate3d(-${value}%, 0, 0), transition: transform ${state.transition}s ease`;
+        transformS;
+        console.log('transformStyle.value :>> ', transformStyle.value);
       }
-      const prev = () => {
-        if(state.currentIndex === 0) return;
-        state.currentIndex -= 1;
-      }
+    );
 
-
-      const next = () => {
-        if(state.currentIndex === images.length - 1) return;
-        state.currentIndex += 1;
-      }
-
-      watch(() => {
-
-      })
-
-
-      return {
-        ...toRefs(state),
-        images,
-      }
-    }
-  }
+    return {
+      ...toRefs(state),
+      images,
+      prev,
+      next,
+      transformStyle,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .slider {
   position: relative;
   margin: 0 auto;
-  /* overflow: hidden; */
+  overflow: hidden;
   border: 2px dashed black;
   background-color: black;
-  height: 400px;
+  height: auto;
   .sliderContent {
     display: flex;
     width: 100%;
     height: 100%;
+    // transition: transform 0.45s ease;
     /* li {
       display: flex;
       width: 100%;
@@ -79,6 +90,21 @@ import {ref, reactive, toRefs, watch} from 'vue';
       width: 100%;
       flex-shrink: 1;
     }
+  }
+  .button {
+    position: absolute;
+    border: 2px solid red;
+    z-index: 2;
+  }
+  .button.btn-previous {
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .button.btn-next {
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
   }
 }
 </style>
